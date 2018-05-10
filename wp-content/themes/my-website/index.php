8a6590841a61945bc05b2abd392cc68fbcacdb46
Grese Hyseni <ghyseni@hawk.iit.edu>
2018-05-10 22:35:45 +0200

    get_header, get_footer functions

--- wp-content/themes/my-website/index.php
+++ wp-content/themes/my-website/index.php
@@ -1,49 +1,48 @@
-<?php include 'header.php'; ?>
-  <main id="main" class="about">
+<?php get_header(); ?>
+<main id="main" class="about">
 
     <article class="content">
-      <h2> Website and Application Developer</h2>
-      <p>
-        I've been working with web development for more than two and a half years now.
-      </p>
-      <p>
-        I've developed quite a broad skill set including: understanding client needs and requirements, team work, time management, project management, technical skills and more.
-      </p>
-      <p>
-        Task driven approach is what I've been using so far to meet the requirements and the deadlines with efficiency.
-      </p>
-      <p>
-        After work, traveling is my passion!
-      </p>
+        <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post();
+                ?>
+                <h2> <?php the_title(); ?> </h2>
+                <?php
+                the_content();
+            endwhile; // end while
+        endif; // end if
+        ?>
+
     </article>
     <section class="skills content">
-      <h3>Skills</h3>
-      <ul>
-        <li>
-          HTML5
-        </li>
-        <li>
-          CSS3
-        </li>
-        <li>
-          JS/Jquery
-        </li>
-        <li>
-          PHP
-        </li>
-        <li>
-          Wordpress
-        </li>
-        <li>
-          NodeJS
-        </li>
-        <li>
-          Java
-        </li>
-      </ul>
-      <span class="bg-image">
-      </span>
+        <h3>Skills</h3>
+        <ul>
+            <li>
+                HTML5
+            </li>
+            <li>
+                CSS3
+            </li>
+            <li>
+                JS/Jquery
+            </li>
+            <li>
+                PHP
+            </li>
+            <li>
+                Wordpress
+            </li>
+            <li>
+                NodeJS
+            </li>
+            <li>
+                Java
+            </li>
+        </ul>
+        <span class="bg-image">
+        </span>
     </section>
 
-  </main>
-<?php include 'footer.php' ?>
+</main>
+<?php get_footer(); ?>
