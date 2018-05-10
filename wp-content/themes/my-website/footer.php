e3ddd49b8b6bc9ab4b6b421e5704cadcab7b60d7
Grese Hyseni <ghyseni@hawk.iit.edu>
2018-05-10 22:34:46 +0200

    Footer wp_footer, display Address using ACF plugin field 'address'

--- wp-content/themes/my-website/footer.php
+++ wp-content/themes/my-website/footer.php
@@ -1,18 +1,28 @@
+
 <footer id="footer">
     <section class="copyright">
         &copy; 2017 Copyright - Grese Hyseni
     </section>
     <section class="contact-info">
-        <address class="address">
-            3241 S Wabash Ave, Chicago, IL, USA
-        </address>
+        <?php
+        $frontpage_id = get_option('page_on_front');
+        $address = get_field('address', $frontpage_id);
+        if ($address):
+            ?>
+            <address class="address">
+                <?php echo $address; ?>
+            </address>
+            <?php
+        endif;
+        ?>
         <a class="phone" href="tel:+19174598826">(+1)917-459-8826</a>
         <a class="phone" href="tel:+38649777826">(+386)49 777 826</a>
         <a class="email" href="mailto:hysenigresa@gmail.com">hysenigresa@gmail.com</a>
     </section>
 </footer>
-<script src="../js/jquery-3.2.1.min.js"></script>
-<script src="../js/app.js"></script>
+<?php wp_footer(); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/frontend/js/jquery-3.2.1.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/frontend/js/app.js"></script>
 
 </body>
 
